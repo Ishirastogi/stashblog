57d30437b3a1b063d8e918648ea63a639d4eec8b
Ishi Rastogi <ishi.rastogi@icloud.com>
2025-09-23 22:34:22 +0530

    catalog1

--- src/pages/catalog1/index.jsx
+++ src/pages/catalog1/index.jsx
@@ -4,7 +4,176 @@ import Footer from 'layouts/home/footer/index.jsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGripHorizontal, faList } from '@fortawesome/free-solid-svg-icons'
 
-
+const products = [
+  {
+    id: 1,
+    name: 'Ceramic Vase',
+    sku: '#90234',
+    image: 'https://images.pexels.com/photos/1099816/pexels-photo-1099816.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 2,
+    name: 'Clay Pot',
+    sku: '#90235',
+    image: 'https://images.pexels.com/photos/1350789/pexels-photo-1350789.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 3,
+    name: 'Decorative Bowl',
+    sku: '#90236',
+    image: 'https://images.pexels.com/photos/102129/pexels-photo-102129.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 4,
+    name: 'Sculpture',
+    sku: '#90237',
+    image: 'https://images.pexels.com/photos/133919/pexels-photo-133919.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 5,
+    name: 'Flower Vase',
+    sku: '#90238',
+    image: 'https://images.pexels.com/photos/236095/pexels-photo-236095.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 6,
+    name: 'Wooden Bowl',
+    sku: '#90239',
+    image: 'https://images.pexels.com/photos/279648/pexels-photo-279648.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 1,
+    name: 'Ceramic Vase',
+    sku: '#90234',
+    image: 'https://images.pexels.com/photos/1099816/pexels-photo-1099816.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 2,
+    name: 'Clay Pot',
+    sku: '#90235',
+    image: 'https://images.pexels.com/photos/1350789/pexels-photo-1350789.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 3,
+    name: 'Decorative Bowl',
+    sku: '#90236',
+    image: 'https://images.pexels.com/photos/102129/pexels-photo-102129.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 4,
+    name: 'Sculpture',
+    sku: '#90237',
+    image: 'https://images.pexels.com/photos/133919/pexels-photo-133919.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 5,
+    name: 'Flower Vase',
+    sku: '#90238',
+    image: 'https://images.pexels.com/photos/236095/pexels-photo-236095.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 6,
+    name: 'Wooden Bowl',
+    sku: '#90239',
+    image: 'https://images.pexels.com/photos/279648/pexels-photo-279648.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 1,
+    name: 'Ceramic Vase',
+    sku: '#90234',
+    image: 'https://images.pexels.com/photos/1099816/pexels-photo-1099816.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 2,
+    name: 'Clay Pot',
+    sku: '#90235',
+    image: 'https://images.pexels.com/photos/1350789/pexels-photo-1350789.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 3,
+    name: 'Decorative Bowl',
+    sku: '#90236',
+    image: 'https://images.pexels.com/photos/102129/pexels-photo-102129.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 4,
+    name: 'Sculpture',
+    sku: '#90237',
+    image: 'https://images.pexels.com/photos/133919/pexels-photo-133919.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 5,
+    name: 'Flower Vase',
+    sku: '#90238',
+    image: 'https://images.pexels.com/photos/236095/pexels-photo-236095.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 6,
+    name: 'Wooden Bowl',
+    sku: '#90239',
+    image: 'https://images.pexels.com/photos/279648/pexels-photo-279648.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 1,
+    name: 'Ceramic Vase',
+    sku: '#90234',
+    image: 'https://images.pexels.com/photos/1099816/pexels-photo-1099816.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 2,
+    name: 'Clay Pot',
+    sku: '#90235',
+    image: 'https://images.pexels.com/photos/1350789/pexels-photo-1350789.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 3,
+    name: 'Decorative Bowl',
+    sku: '#90236',
+    image: 'https://images.pexels.com/photos/102129/pexels-photo-102129.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 4,
+    name: 'Sculpture',
+    sku: '#90237',
+    image: 'https://images.pexels.com/photos/133919/pexels-photo-133919.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 5,
+    name: 'Flower Vase',
+    sku: '#90238',
+    image: 'https://images.pexels.com/photos/236095/pexels-photo-236095.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 6,
+    name: 'Wooden Bowl',
+    sku: '#90239',
+    image: 'https://images.pexels.com/photos/279648/pexels-photo-279648.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+    {
+    id: 3,
+    name: 'Decorative Bowl',
+    sku: '#90236',
+    image: 'https://images.pexels.com/photos/102129/pexels-photo-102129.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 4,
+    name: 'Sculpture',
+    sku: '#90237',
+    image: 'https://images.pexels.com/photos/133919/pexels-photo-133919.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 5,
+    name: 'Flower Vase',
+    sku: '#90238',
+    image: 'https://images.pexels.com/photos/236095/pexels-photo-236095.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  },
+  {
+    id: 6,
+    name: 'Wooden Bowl',
+    sku: '#90239',
+    image: 'https://images.pexels.com/photos/279648/pexels-photo-279648.jpeg?auto=compress&cs=tinysrgb&w=300&h=300'
+  }
+]
 const CatalogPage = () => {
   return (
     <div>
@@ -50,6 +219,20 @@ const CatalogPage = () => {
           </div>
         </div>
       </div>
+
+      <main className={s.mainContent}>
+        <div className={s.productGrid}>
+          {products.map(product => (
+            <div key={product.id} className={s.productItem}>
+              <img src={product.image} alt={product.name} />
+              <div className={s.productText}>
+                <h3>{product.name}</h3>
+                <span className={s.sku}>{product.sku}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
 
       <Footer />
     </div>
